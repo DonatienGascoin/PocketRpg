27017a833a50dc336da4b8d1b005679bf23915dc
Donatien <donatien.gascoin@gmail.com>
2025-12-30 21:59:39 +0100

    Claude update, font definition start

---
+++ src/main/java/com/pocket/rpg/ui/text/FontDefinition.java
@@ -0,0 +1,13 @@
+package com.pocket.rpg.ui.text;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FontDefinition {
+    private String fontFile;
+    private int size;
+}
